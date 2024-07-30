ea76447c164462a9b83977e0520a329200a71fec
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-07-30 20:18:26 +0300

    first commit

---
+++ src/test/java/com/workintech/e_commercebackend/ECommercebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.e_commercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
